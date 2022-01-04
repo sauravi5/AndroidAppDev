5242083571ed81983bf4924f04b22c2e5617eded
sauravi5 <97092518+sauravi5@users.noreply.github.com>
2022-01-04 15:33:55 +0530

    Update Sum.java

--- Assignment 2/Array/Sum.java
+++ Assignment 2/Array/Sum.java
@@ -1,1 +1,15 @@
+package com.mpstme;
 
+public class Sum{  
+    public static void main(String[] args) 
+    {  
+        int [] arr = new int [] {1, 2, 3};  
+        int sum = 0;  
+        
+        for (int i = 0; i < arr.length; i++) 
+        {  
+           sum = sum + arr[i];  
+        }  
+        System.out.println("Sum of all the elements of an array: " + sum);  
+    }  
+}  
