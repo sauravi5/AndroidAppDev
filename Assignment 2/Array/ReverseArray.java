b808ea8ab58f24c3223baca5c383d347d46fcbc3
sauravi5 <97092518+sauravi5@users.noreply.github.com>
2022-01-04 15:38:42 +0530

    Update ReverseArray.java

--- Assignment 2/Array/ReverseArray.java
+++ Assignment 2/Array/ReverseArray.java
@@ -3,7 +3,7 @@ package com.mpstme
 public class ReverseArray {  
     public static void main(String[] args) 
     {  
-        int [] arr = new int [] {1, 2, 3, 4, 5};  
+        int [] arr = new int [] {1, 2, 3, 4, 5, 6, 7};  
         System.out.println("Original array: ");  
         for (int i = 0; i < arr.length; i++) 
         {  
