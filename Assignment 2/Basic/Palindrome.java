f39ee2f1da34cdbe7afae8ea92dc8a0068348037
sauravi5 <97092518+sauravi5@users.noreply.github.com>
2022-01-04 15:52:58 +0530

    Create Palindrome.java

---
+++ Assignment 2/Basic/Palindrome.java
@@ -0,0 +1,21 @@
+package com.mpstme
+
+class Palindrome{  
+ public static void main(String args[])
+ {  
+  int r,sum=0,temp;    
+  int n=34543;  
+  
+  temp=n;    
+  while(n>0)
+  {    
+   r=n%10; 
+   sum=(sum*10)+r;    
+   n=n/10;    
+  }    
+  if(temp==sum)    
+   System.out.println("palindrome number ");    
+  else    
+   System.out.println("not palindrome");    
+}  
+}  
